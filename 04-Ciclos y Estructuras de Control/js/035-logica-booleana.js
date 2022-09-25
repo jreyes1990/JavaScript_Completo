42ace04b3119fc175d3c6afcced6ce9deeaeca58
Jolberth Reyes <jarl1990mt@gmail.com>
2022-09-25 01:02:04 -0600

    Operador logico booleano

--- 04-Ciclos y Estructuras de Control/js/035-logica-booleana.js
+++ 04-Ciclos y Estructuras de Control/js/035-logica-booleana.js
@@ -19,3 +19,30 @@ console.log(!false) //true
 
 console.log(!regresaFalse()) //true
 
+console.warn('And')
+console.log(true && true)
+console.log(true && !false)
+
+console.log('==========')
+console.log(regresaFalse() && regresaTrue()) //false
+console.log(regresaTrue() && regresaFalse()) //false
+
+console.log('===== && =====')
+regresaFalse() && regresaTrue()
+
+console.log('4 condiciones', true && true && true && false)
+
+console.log('===== or =====')
+console.log(true || false)
+console.log(false || false)
+console.log(regresaTrue() || regresaFalse())
+console.log('4 condiciones', true || true || true || false)
+
+console.warn('Asignaciones')
+const suyUndefined = undefined;
+const soyNull = null;
+const soyFalso =false;
+
+const a1 = false && 'Hola Mundo' && 150;
+
+console.log({ a1 })
