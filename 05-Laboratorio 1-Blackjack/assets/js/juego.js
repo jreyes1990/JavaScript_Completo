1640815c1883d56962a027be97c30ee8598c1bde
Jolberth Reyes <jarl1990mt@gmail.com>
2022-09-27 21:33:17 -0600

    Logica del juego de cartas

---
+++ 05-Laboratorio 1-Blackjack/assets/js/juego.js
@@ -0,0 +1,83 @@
+/*
+*  2C = Two of Clubs
+*  2D = Two of Diamonds
+*  2H = Two of Hearts
+*  2S = Two of Spades
+*  
+*  http://underscorejs.org/
+*/
+
+
+let deck = []; 
+let tipos = ['C','D','H','S'];
+let especiales = ['A','J','Q','K'];
+
+// Declarando variables de puntos
+let puntosJugador = 0, puntosComputadora = 0;
+
+// Referencial del HTML
+const btnPedir = document.querySelector("#btnPedir");
+const divCartasJugador = document.querySelector('#jugador-cartas');
+const puntosHTML = document.querySelectorAll('small');
+
+const crearDeck = () => {
+    for(let i=2; i<=10; i++ ){
+        for(let tipo of tipos){
+            deck.push(i + tipo);
+        }
+    }
+
+    for(let tipo of tipos){
+        for(let esp of especiales){
+            deck.push(esp + tipo);
+        }
+    }
+
+    deck = _.shuffle(deck);
+
+    return deck;
+}
+
+crearDeck();
+
+// Esta funcion me permite tomar una carta
+const pedirCarta = () => {
+    if (deck.length === 0){
+        throw 'No hay cartas en el deck'
+    }
+
+    const carta = deck.pop();
+
+    return carta;
+}
+
+//pedirCarta();
+
+const valorCarta = ( carta ) => {
+    const valor = carta.substring(0, carta.length - 1);
+    
+    return ( isNaN(valor) ) ? (valor === 'A') ? 11 : 10 : valor * 1;
+}
+
+//Turno de la computadora
+
+// Eventos
+btnPedir.addEventListener('click',() => {
+    const carta = pedirCarta();
+
+    puntosJugador = puntosJugador + valorCarta(carta);
+    puntosHTML[0].innerText = puntosJugador;
+
+    const imgCarta = document.createElement('img');
+    imgCarta.src = `assets/cartas/${carta}.png`;
+    imgCarta.classList.add('carta');
+    divCartasJugador.append(imgCarta);
+
+    if(puntosJugador > 21){
+        console.warn('Lo siento mucho, perdite')
+        btnPedir.disabled = true;
+    }else if(puntosJugador === 21){
+        console.log('21, genial haz ganado')
+        btnPedir.disabled = true;
+    }
+});
