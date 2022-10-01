33c90c5777b46d7b47d2c105cb3fc3a31b0fb893
Jolberth Reyes <jarl1990mt@gmail.com>
2022-09-30 20:46:57 -0600

    Construccion de clases, contructor y sets

---
+++ 07-Clases y ESNext private properties/assets/js/classes/class.js
@@ -0,0 +1,34 @@
+class Persona {
+    nombre = '';
+    codigo = '';
+    frase = '';
+    comida = '';
+
+    constructor(nombre='Sin nombre', codigo='Sin codigo',frase='Sin frase'){ //Metodo que se ejecuta mediante una instancia
+        this.nombre = nombre
+        this.codigo = codigo
+        this.frase = frase
+    }
+
+    set setComidaFavorita(comida){
+        this.comida = comida;
+    }
+
+    quienSoy(){
+        console.log(`Soy ${this.nombre} y mi identidad es ${this.codigo}`)
+    }
+
+    miFrase(){
+        this.quienSoy();
+        console.log(`${this.codigo} dice: ${this.frase}`)
+    }
+}
+
+const spiderman = new Persona('Peter Parker','Spiderman','Soy tu amable amigo Spiderman');
+const iroman = new Persona('Tony Stark','Iroman','Yo soy Iroman');
+
+
+console.log(iroman)
+console.log(spiderman)
+
+spiderman.setComidaFavorita = 'El pie de cereza de la tia May';
