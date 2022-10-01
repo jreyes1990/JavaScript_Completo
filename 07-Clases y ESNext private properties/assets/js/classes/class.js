b240992c596627b7715dd15b268fb0634e782e4d
Jolberth Reyes <jarl1990mt@gmail.com>
2022-09-30 22:30:01 -0600

    Propiedades gets y metodos staticos

--- 07-Clases y ESNext private properties/assets/js/classes/class.js
+++ 07-Clases y ESNext private properties/assets/js/classes/class.js
@@ -1,13 +1,21 @@
 class Persona {
+    static _conteo = 0;
+
+    static get conte(){
+        return Persona._conteo + ' instancias';
+    }
+
     nombre = '';
     codigo = '';
     frase = '';
     comida = '';
 
     constructor(nombre='Sin nombre', codigo='Sin codigo',frase='Sin frase'){ //Metodo que se ejecuta mediante una instancia
-        this.nombre = nombre
-        this.codigo = codigo
-        this.frase = frase
+        this.nombre = nombre;
+        this.codigo = codigo;
+        this.frase = frase;
+
+        Persona._conteo++;
     }
 
     set setComidaFavorita(comida){
@@ -29,12 +37,15 @@ class Persona {
 }
 
 const spiderman = new Persona('Peter Parker','Spiderman','Soy tu amable amigo Spiderman');
-const iroman = new Persona('Tony Stark','Iroman','Yo soy Iroman');
+//const iroman = new Persona('Tony Stark','Iroman','Yo soy Iroman');
 
 
-console.log(iroman)
+//console.log(iroman)
 console.log(spiderman)
 
 spiderman.setComidaFavorita = 'El pie de cereza de la tia May';
 
 console.log(spiderman.getComidaFavorita)
+
+console.log('Conteo Stático Persona(s): ',Persona._conteo)
+console.log('Conteo Instancia Persona(s): ',Persona.conte)
