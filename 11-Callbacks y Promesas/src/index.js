009077e7419d5a92ce65b59baaa71aa72dbd65ed
Jolberth Reyes <jarl1990mt@gmail.com>
2022-10-06 22:23:32 -0600

    Callback Hell

--- 11-Callbacks y Promesas/src/index.js
+++ 11-Callbacks y Promesas/src/index.js
@@ -1,13 +1,14 @@
 import {buscarHeroe} from './js/callbacks';
 import './styles.css';
 
-const heroeId = 'capi';
+const heroeId1 = 'capi';
+const heroeId2 = 'iron';
 
-buscarHeroe(heroeId, (err, heroe) => {
-    if(err){
-        console.error(err)
-    }else{
-        console.info(heroe)
-    }
+buscarHeroe(heroeId1, (err, heroe1) => {
+    if(err){ return console.error(err) }
     
+    buscarHeroe(heroeId2, (err, heroe2) => {
+        if(err){ return console.error(err) }
+        console.log(`Enviando a ${heroe1.nombre} y ${heroe2.nombre} a la mision`)
+    });
 });
