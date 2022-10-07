ffc6d5ee83cd0890e0708a4396d527cc0d267dec
Jolberth Reyes <jarl1990mt@gmail.com>
2022-10-07 00:33:44 -0600

    Promise race

--- 11-Callbacks y Promesas/src/index.js
+++ 11-Callbacks y Promesas/src/index.js
@@ -1,2 +1,6 @@
+import {promesaLenta,promesaMedia,promesaRapida} from './js/promesas';
 import './styles.css';
 
+Promise.race([promesaLenta,promesaMedia,promesaRapida])
+        .then(console.log)
+        .catch(console.warn);
