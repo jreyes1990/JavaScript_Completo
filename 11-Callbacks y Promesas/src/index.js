6b2b3bee279f736b473e0450b869ab74eec6c0cb
Jolberth Reyes <jarl1990mt@gmail.com>
2022-10-07 23:35:09 -0600

    Await

--- 11-Callbacks y Promesas/src/index.js
+++ 11-Callbacks y Promesas/src/index.js
@@ -1,7 +1,15 @@
-//import {promesaLenta,promesaMedia,promesaRapida} from './js/promesas';
-import {buscarHeroe,buscarHeroeAsync} from './js/promesas';
+import { obtenerHeroesArr } from './js/await';
 import './styles.css';
+//import {promesaLenta,promesaMedia,promesaRapida} from './js/promesas';
+//import {buscarHeroe,buscarHeroeAsync} from './js/promesas';
 
+
+
+const heroes = obtenerHeroesArr();
+
+console.table(heroes)
+
+/*
 buscarHeroe('capi2')
         .then(heroe => console.log(heroe))
         .catch(console.warn);
@@ -9,7 +17,7 @@ buscarHeroe('capi2')
 buscarHeroeAsync('iron2')
         .then(heroe => console.log(heroe))
         .catch(console.warn);
-
+*/
 
 /*
 Promise.race([promesaLenta,promesaMedia,promesaRapida])
