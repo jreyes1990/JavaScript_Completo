8e3c11ba4b6f1d312df1849c1ddffa28badd4334
Jolberth Reyes <jarl1990mt@gmail.com>
2022-10-08 22:03:50 -0600

    CRUD - Leer Usuarios

--- 12-Peticiones HTTP y upload archivos/src/index.js
+++ 12-Peticiones HTTP y upload archivos/src/index.js
@@ -1,13 +1,15 @@
 //import './styles.css';
 //import { saludar } from './js/componentes.js';
 //import { obtenerChiste } from './js/http-privider';
-import { init } from "./js/usuarios-page";
+//import { init } from "./js/usuarios-page";
 //import { obtenerUsuarios } from "./js/http-privider";
+import * as CRUD from './js/crud_usuarios-provider';
 
+CRUD.getUsuario(2).then(console.log);
 
 //obtenerUsuarios().then(console.log);
 
-init();
+//init();
 
 //obtenerChiste().then(console.log);
 
